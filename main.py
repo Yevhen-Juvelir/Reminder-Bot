d68a3892fa0b7fbda011ab6815abc136287c9f4d
Yevhen <yevhen.kozirovskyi@student.tuke.sk>
2025-10-07 17:52:38 +0200

    qwe

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, Updater
+
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+BOT_TOKEN = os.getenv("TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я твой бот 🤖")
 
-app = ApplicationBuilder().token("8411849882:AAEJNZ9B0WPoxFokfquZlV0Uv-L2Jft1gD8").build()
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+print("BOT started")
+
+print(BOT_TOKEN)
 app.add_handler(CommandHandler("start", start))
 app.run_polling()
